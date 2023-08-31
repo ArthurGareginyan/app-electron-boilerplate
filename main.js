15868ecec3d32837c9befca75d5ac1f0f918cdab
Arthur Gareginyan <arthurgareginyan@gmail.com>
2023-09-01 02:38:36 +0300

    Change: Code formatting improvement

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
+const {
+    app,
+    BrowserWindow
+} = require('electron')
 
-const { app, BrowserWindow } = require('electron')
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        //frame: false,
+        //titleBarStyle: 'hidden',
+        webPreferences: {
+            nodeIntegration: true,
+        }
+    })
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    //frame: false,
-    //titleBarStyle: 'hidden',
-    webPreferences: {
-      nodeIntegration: true,
-    }
-  })
-
-  win.loadFile('index.html')
+    win.loadFile('index.html')
 }
 
 app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
